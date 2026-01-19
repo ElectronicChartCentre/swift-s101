9a0bced61bfc9cd717dd427a954e7dbc8fe17597
Tore Halset <halset@pvv.ntnu.no>
2026-01-19 09:42:53 +0100

    github action build

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        //.package(url: "https://github.com/ElectronicChartCentre/swift-iso8211", from: "1.0.0"),
-        .package(path: "../swift-iso8211"),
+        .package(url: "https://github.com/ElectronicChartCentre/swift-iso8211", branch: "main"),
+        //.package(path: "../swift-iso8211"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.3.0"),
         // zip only needed for testing
         .package(url: "https://github.com/adam-fowler/swift-zip-archive", from: "0.6.4")
